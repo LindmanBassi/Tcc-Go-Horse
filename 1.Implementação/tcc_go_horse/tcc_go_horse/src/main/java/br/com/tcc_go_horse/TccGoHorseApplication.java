4995f4f9a772bfb920295fa3877b446618b19c32
Henrique Lindman Bassi <148555934+LindmanBassi@users.noreply.github.com>
2026-03-12 15:38:33 -0300

    first commit

---
+++ 1.Implementação/tcc_go_horse/tcc_go_horse/src/main/java/br/com/tcc_go_horse/TccGoHorseApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcc_go_horse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccGoHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccGoHorseApplication.class, args);
+	}
+
+}
